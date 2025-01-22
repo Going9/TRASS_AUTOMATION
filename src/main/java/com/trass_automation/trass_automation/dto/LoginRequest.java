b0654bb4a526ab44ed9bc1b7848cbf8b3aefd3ee
Going9 <ickdor123@gmail.com>
2025-01-22 19:03:53 +0900

    DTO: add login request dto

---
+++ src/main/java/com/trass_automation/trass_automation/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.trass_automation.trass_automation.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String id;
+    private String password;
+}
