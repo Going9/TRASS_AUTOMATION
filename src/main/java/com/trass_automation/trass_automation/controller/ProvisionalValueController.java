2fa9754feda8d2054df30bf9175da14d14ba4de9
Going9 <ickdor123@gmail.com>
2025-01-22 19:03:28 +0900

    Feat: add provisional value controller

---
+++ src/main/java/com/trass_automation/trass_automation/controller/ProvisionalValueController.java
@@ -0,0 +1,27 @@
+package com.trass_automation.trass_automation.controller;
+
+import com.trass_automation.trass_automation.dto.LoginRequest;
+import com.trass_automation.trass_automation.service.ProvisionalValueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+// 잠정치 조회 컨트롤러
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/provisional-value")
+public class ProvisionalValueController {
+
+    private final ProvisionalValueService provisionalValueService;
+
+    @PostMapping("/login")
+    public void loginTRASS(@RequestBody LoginRequest request) {
+        provisionalValueService.loginTRASS(request);
+    }
+
+    @GetMapping("/test")
+    public void test() {
+        System.out.println("????");
+    }
+
+}
