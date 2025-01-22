c32db0b11642515d26b68a3be2152cb68830ebc7
Going9 <ickdor123@gmail.com>
2025-01-22 19:07:53 +0900

    Fix: fix headless mode is not applied

---
+++ src/main/java/com/trass_automation/trass_automation/modules/WebDriverFactory.java
@@ -0,0 +1,20 @@
+package com.trass_automation.trass_automation.modules;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebDriverFactory {
+    public WebDriver createHeadlessDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // 최신 Chrome에서는 --headless 대신 --headless=new 사용
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage"); // /dev/shm 사용하지 않음
+        options.addArguments("--remote-allow-origins=*"); // CORS 문제 회피
+
+        return new ChromeDriver(options);
+    }
+}
+
