d5a766348025a0e66e1b6a61ac9d35950ecbcaf2
Going9 <ickdor123@gmail.com>
2025-01-22 14:28:58 +0900

    Init: project TRASS_AUTOMAITON

---
+++ src/main/java/com/trass_automation/trass_automation/TrassAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.trass_automation.trass_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrassAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrassAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trass_automation/trass_automation/TrassAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trass_automation.trass_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrassAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
