d9f04097810088694c60951c3d3d39ed71a49e1a
Going9 <ickdor123@gamil.com>
2025-02-28 15:46:08 +0900

    Feats: adding  detail value page

--- src/main/java/com/trass_automation/trass_automation/controller/PageController.java
+++ src/main/java/com/trass_automation/trass_automation/controller/PageController.java
@@ -1,9 +1,16 @@
 package com.trass_automation.trass_automation.controller;
 
+import com.trass_automation.trass_automation.dto.detailValue.DetailValueOfTwoItemsRequest;
+import com.trass_automation.trass_automation.dto.detailValue.DetailValueOfTwoItemsResponseWrapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,4 +23,10 @@ public class PageController {
         return"index-form";
     }
 
+    @PostMapping("/detail-value")
+    public String fetchDetailValueOfTwoItems(@ModelAttribute DetailValueOfTwoItemsRequest request, Model model) throws IOException {
+        DetailValueOfTwoItemsResponseWrapper detailValueOfTwoItemsResponseWrapper = detailValueController.getDetailValueOfTwoItems(request);
+        model.addAttribute("detailValueOfTwoItemsResponseWrapper", detailValueOfTwoItemsResponseWrapper);
+        return "detail-value-list";
+    }
 }
